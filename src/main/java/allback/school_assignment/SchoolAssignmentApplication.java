efcf2f4d95f6bfd7b6eb1a30ac8320403ee6f01c
seongbeom_park <seongbeom_park@sparkamita90.iptime.org>
2023-08-25 11:46:32 +0900

    first commit

---
+++ src/main/java/allback/school_assignment/SchoolAssignmentApplication.java
@@ -0,0 +1,13 @@
+package allback.school_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/allback/school_assignment/SchoolAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package allback.school_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
