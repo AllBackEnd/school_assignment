84d4375220e0900072702991b5b84228e99d6187
ysd3392 <seongdae.yu@silcroadsoft.com>
2023-08-25 20:41:08 +0900

    신규 class, enum 추가

---
+++ src/main/java/allback/school_assignment/algorithm/SchoolMapper.java
@@ -0,0 +1,37 @@
+package allback.school_assignment.algorithm;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class SchoolMapper {
+
+  // 배정 학교 이름을 지망 순위로 변환
+  public Map<Integer, PreferEnum> map(Map<Integer, String> allocation, Map<Integer, List<String>> engageList) {
+    Map<Integer, PreferEnum> result = new HashMap<>();
+
+    Iterator iterator = allocation.entrySet().iterator();
+    while (iterator.hasNext()) {
+      Entry<Integer, String> allocElement = (Entry<Integer, String>) iterator.next();
+      Integer stdId = allocElement.getKey();
+      String allocSchool = allocElement.getValue();
+
+      result.put(stdId, findPrefer(allocSchool, engageList.get(stdId)));
+    }
+
+    return result;
+  }
+
+  // 지망 순위 계산
+  private PreferEnum findPrefer(String allocSchool, List<String> engageList) {
+    for (int i = 0; i < engageList.size(); i++) {
+      if (allocSchool.equals(engageList.get(i))) {
+        return PreferEnum.valueOf(i);
+      }
+    }
+
+    return PreferEnum.RANDOM;
+  }
+}
