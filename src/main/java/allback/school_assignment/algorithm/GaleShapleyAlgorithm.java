284ee04903841dd4ee361ba47a9d80d7802cb5e6
ysd3392 <seongdae.yu@silcroadsoft.com>
2023-08-25 17:22:36 +0900

    1차 완성

--- src/main/java/allback/school_assignment/algorithm/GaleShapleyAlgorithm.java
+++ src/main/java/allback/school_assignment/algorithm/GaleShapleyAlgorithm.java
@@ -21,62 +21,62 @@ public class GaleShapleyAlgorithm {
     put(S_FOU, 3);
   }};
   public static Map<Integer, List<String>> STUDENT_DATA = new HashMap<Integer, List<String>>() {{
-    put(1, new ArrayList<String>() {{
+    put(3001, new ArrayList<String>() {{
       add(S_ONE);
       add(S_TWO);
       add(S_THR);
     }});
-    put(2, new ArrayList<String>() {{
+    put(3002, new ArrayList<String>() {{
       add(S_TWO);
       add(S_THR);
       add(S_FOU);
     }});
-    put(3, new ArrayList<String>() {{
+    put(3003, new ArrayList<String>() {{
       add(S_ONE);
       add(S_THR);
       add(S_FOU);
     }});
-    put(4, new ArrayList<String>() {{
-      add(S_ONE);
-      add(S_TWO);
-      add(S_FOU);
-    }});
-    put(5, new ArrayList<String>() {{
-      add(S_ONE);
-      add(S_TWO);
-      add(S_THR);
-    }});
-    put(6, new ArrayList<String>() {{
-      add(S_TWO);
-      add(S_THR);
-      add(S_FOU);
-    }});
-    put(7, new ArrayList<String>() {{
-      add(S_ONE);
-      add(S_THR);
-      add(S_FOU);
-    }});
-    put(8, new ArrayList<String>() {{
+    put(3004, new ArrayList<String>() {{
       add(S_ONE);
       add(S_TWO);
       add(S_FOU);
     }});
-    put(9, new ArrayList<String>() {{
+    put(2001, new ArrayList<String>() {{
       add(S_ONE);
       add(S_TWO);
       add(S_THR);
     }});
-    put(10, new ArrayList<String>() {{
+    put(2002, new ArrayList<String>() {{
       add(S_TWO);
       add(S_THR);
       add(S_FOU);
     }});
-    put(11, new ArrayList<String>() {{
+    put(2003, new ArrayList<String>() {{
       add(S_ONE);
       add(S_THR);
       add(S_FOU);
     }});
-    put(12, new ArrayList<String>() {{
+    put(2004, new ArrayList<String>() {{
+      add(S_ONE);
+      add(S_TWO);
+      add(S_FOU);
+    }});
+    put(1001, new ArrayList<String>() {{
+      add(S_ONE);
+      add(S_TWO);
+      add(S_THR);
+    }});
+    put(1002, new ArrayList<String>() {{
+      add(S_TWO);
+      add(S_THR);
+      add(S_FOU);
+    }});
+    put(1003, new ArrayList<String>() {{
+      add(S_ONE);
+      add(S_THR);
+      add(S_FOU);
+    }});
+    put(1004, new ArrayList<String>() {{
       add(S_ONE);
       add(S_TWO);
       add(S_FOU);
@@ -89,48 +89,92 @@ public class GaleShapleyAlgorithm {
     add(S_FOU);
   }};
   public static ArrayList<Integer> STUDENT_IDS = new ArrayList<Integer>() {{
-    add(1);
-    add(2);
-    add(3);
-    add(4);
-    add(5);
-    add(6);
-    add(7);
-    add(8);
-    add(9);
-    add(10);
-    add(11);
-    add(12);
+    add(3001);
+    add(3002);
+    add(3003);
+    add(3004);
+    add(2001);
+    add(2002);
+    add(2003);
+    add(2004);
+    add(1001);
+    add(1002);
+    add(1003);
+    add(1004);
   }};
+
+  private SchoolCurCntMap schoolCurCntMap;
+  private boolean [] entered;
+  @SuppressWarnings("unchecked")
+  private Map<Integer, String> allocation;
+  @SuppressWarnings("unchecked")
+  private Map<String, Integer> remain_schools;
+  @SuppressWarnings("unchecked")
+  private List<Integer> remain_students;
 
   public String calculate(List<String> school_ids, List<Integer> student_ids) {
     // 1. data 확보
-    Map<String, Integer> schools = getAllSchool(); // 학교 이름과 정원
-    Map<Integer, List<String>> students = getAllStudentList(); // 학생 id와 지망 리스트
+    Map<String, Integer> schoolsMaxCntList = getAllSchool(); // 학교 이름과 정원
+    Map<Integer, List<String>> studentsEngageList = getAllStudentList(); // 학생 id와 지망 리스트
     int n_wishes = 3; // 지망 순위 개수, 위 list의 lenth
     Map<Integer, List<String>> forbidden = getAllForbidden(); // 매칭 금지 리스트, 무조건 비어있음
 
     // 2. 연산 수행
-    List<Object> result = allocate(schools, students, n_wishes, forbidden, school_ids, student_ids);
+    allocate(schoolsMaxCntList, studentsEngageList, n_wishes, forbidden, school_ids, student_ids);
 
     // 3. 결과 출력
-    return printResult(result);
+    return printResult();
   }
 
-  private List<Object> allocate(Map<String, Integer> schools, Map<Integer, List<String>> students,
-      int n_wishes, Map<Integer, List<String>> forbidden, List<String> school_ids,
-      List<Integer> student_ids) {
+  private void allocate(Map<String, Integer> schoolsMaxCntList,
+      Map<Integer, List<String>> studentsEngageList, int n_wishes,
+      Map<Integer, List<String>> forbidden, List<String> school_ids, List<Integer> student_ids) {
 
+    // 1. 학교 별로 현재 입학 인원과 최대 정원을 관리하는 class의 객체 생성, 기타 초기화 작업
+    schoolCurCntMap = new SchoolCurCntMap(schoolsMaxCntList);
+    entered = new boolean[student_ids.size()];
+    allocation = new HashMap<>();
+    remain_schools = new HashMap<>();
+    remain_students = new ArrayList<>();
 
+    // 2. 모든 학생이 가장 높은 학교부터 지원
+    for (int prefer = 0; prefer < n_wishes; prefer++) {
+      // 3. 각 학생이 list에 들어간 순서대로 학교를 지원
+      for (int stdIdx = 0; stdIdx < studentsEngageList.size(); stdIdx++) {
+        // 4. 이미 지원이 완료된 학생이라면 skip
+        if (isEntered(stdIdx)) {
+          break;
+        }
 
-    return null;
+        // 5. 특정 학생이 현재 우선 순위에서 원하는 학교 획득
+        String preferSchool = getPreferSchool(prefer, stdIdx, studentsEngageList);
+        
+        // 6. 해당 학교에 지원
+        if (schoolCurCntMap.isRemain(preferSchool)) {
+          enterSchool(preferSchool, stdIdx, student_ids);
+        }
+      }
+    }
   }
 
-  private String printResult(List<Object> result) {
-    @SuppressWarnings("unchecked") Map<Integer, String> allocation = (Map<Integer, String>) result.get(0);
-    @SuppressWarnings("unchecked") Map<String, Integer> remain_schools = (Map<String, Integer>) result.get(1);
-    @SuppressWarnings("unchecked") List<Integer> remain_students = (List<Integer>) result.get(2);
+  // 지원 완료 여부 판단
+  private boolean isEntered(int stdIdx) {
+    return  (entered[stdIdx]);
+  }
 
+  // 현재 우선 순위에서 선호 학교명 획득
+  private String getPreferSchool(int prefer, int stdIdx, Map<Integer, List<String>> studentsEngageList) {
+    return studentsEngageList.get(stdIdx).get(prefer);
+  }
+
+  // 학교에 지원 (지원 가능한 상태에서만 호출됨)
+  private void enterSchool(String preferSchool, int stdIdx, List<Integer> school_ids) {
+    schoolCurCntMap.increaseCurCnt(preferSchool);
+    entered[stdIdx] = true;
+    allocation.put(school_ids.get(stdIdx), preferSchool);
+  }
+
+  private String printResult() {
     System.out.println("Policy: GaleShapleyAlgorithm" +
         ", seed: 0 " +
         ", allocation: " + allocation +
@@ -155,10 +199,11 @@ public class GaleShapleyAlgorithm {
     return new HashMap<Integer, List<String>>();
   }
 
+  /*
   @GetMapping("/alloc")
   public String alloc(@RequestParam(value = "policy", defaultValue = "Hybrid") String policy,
       @RequestParam(value = "seed", defaultValue = "0") long seed) {
-    /*
+
     Map<String, Integer> schools = new HashMap<String, Integer>() {{ put("a", 1); put("b", 1); put("c", 1); }};
     Map<Integer, List<String>> students = new HashMap<Integer, List<String>>() {{
       put(1, new ArrayList<String>() {{ add("a"); add("c"); }});
@@ -184,7 +229,8 @@ public class GaleShapleyAlgorithm {
         ", remain_schools: " + remain_schools +
         ", remain_students: " + remain_students);
     return String.format("Policy %s!", policy);
-    */
+
     return null;
   }
+  */
 }
---
+++ src/main/java/allback/school_assignment/algorithm/SchoolCurCntMap.java
@@ -0,0 +1,43 @@
+package allback.school_assignment.algorithm;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class SchoolCurCntMap {
+
+  private Map<String, List<Integer>> map = new HashMap<>();
+  private static Integer CUR_IDX = 0;
+  private static Integer MAX_IDX = 0;
+
+  public SchoolCurCntMap(Map<String, Integer> schools) {
+    Iterator iterator = schools.entrySet().iterator();
+    while (iterator.hasNext()) {
+      Entry<String, Integer> element = (Entry<String, Integer>) iterator.next();
+      map.put(element.getKey(), new ArrayList<>() {{ add(0); add(element.getValue());}});
+    }
+  }
+
+  private int getCurCnt(String key) {
+    return map.get(key).get(CUR_IDX);
+  }
+
+  private int getMaxCnt(String key) {
+    return map.get(key).get(MAX_IDX);
+  }
+
+  public void increaseCurCnt(String key) {
+    map.get(key).set(CUR_IDX, getCurCnt(key) + MAX_IDX);
+  }
+
+  public boolean isRemain(String key) {
+    if (getCurCnt(key) < getMaxCnt(key)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
